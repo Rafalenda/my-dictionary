41ea1b1f816ec8ad2b4afe6d900808e2886f7248
Rafaela Almeida Da Silva <rafaela.almeida.silva@gmail.com>
2021-05-08 14:20:49 -0400

    Set up the dictionary app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Dictionary</p>
+        <button className="btn btn-primary">Search</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
