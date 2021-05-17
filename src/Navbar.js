581a575475d409b2d99f6a5b52059c9568ec3111
Rafaela Almeida Da Silva <rafaela.almeida.silva@gmail.com>
2021-05-17 17:13:15 -0400

    Navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -16,7 +16,7 @@ export default function Navbar() {
         aria-expanded="false"
         aria-label="Toggle navigation"
       >
-        <span class="navbar-toggler-icon"></span>
+        <span className="navbar-toggler-icon"></span>
       </button>
 
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
